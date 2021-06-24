18e631c220e3ff719f1476b3f966914b8da9abc7
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2021-06-24 16:31:03 +0200

    plz fix

--- src/main/java/com/enderzombi102/mception/block/ComputerBlock.java
+++ src/main/java/com/enderzombi102/mception/block/ComputerBlock.java
@@ -1,18 +1,22 @@
 package com.enderzombi102.mception.block;
 
+import com.enderzombi102.mception.screen.ComputerScreenHandler;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
-import net.fabricmc.fabric.api.tag.FabricTag;
-import net.fabricmc.fabric.api.tag.FabricTagBuilder;
 import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags;
 import net.minecraft.block.*;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.inventory.SimpleInventory;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.item.ItemStack;
 import net.minecraft.screen.NamedScreenHandlerFactory;
+import net.minecraft.screen.ScreenHandler;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.state.property.Properties;
+import net.minecraft.text.LiteralText;
+import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
@@ -21,7 +25,7 @@ import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
-public class ComputerBlock extends HorizontalFacingBlock implements BlockEntityProvider {
+public class ComputerBlock extends HorizontalFacingBlock implements BlockEntityProvider, NamedScreenHandlerFactory {
 
 	public static final BooleanProperty ACTIVE = BooleanProperty.of("active");
 
@@ -43,13 +47,13 @@ public class ComputerBlock extends HorizontalFacingBlock implements BlockEntityP
 
 	@Override
 	public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
-		if (!world.isClient) {
+		if (! world.isClient ) {
 			//This will call the createScreenHandlerFactory method from BlockWithEntity, which will return our blockEntity casted to
 			//a namedScreenHandlerFactory. If your block class does not extend BlockWithEntity, it needs to implement createScreenHandlerFactory.
 			NamedScreenHandlerFactory screenHandlerFactory = state.createScreenHandlerFactory(world, pos);
 
 			if (screenHandlerFactory != null) {
-				//With this call the server will request the client to open the appropriate Screenhandler
+				// With this call the server will request the client to open the appropriate ScreenHandler
 				player.openHandledScreen(screenHandlerFactory);
 			}
 		}
@@ -82,5 +86,18 @@ public class ComputerBlock extends HorizontalFacingBlock implements BlockEntityP
 	@Override
 	public BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
 		return new ComputerBlockEntity(pos, state);
+	}
+
+	// named screen handled factory
+
+	@Override
+	public Text getDisplayName() {
+		return new LiteralText("Computer Screen");
+	}
+
+	@Nullable
+	@Override
+	public ScreenHandler createMenu(int syncId, PlayerInventory inv, PlayerEntity player) {
+		return new ComputerScreenHandler(syncId, inv);
 	}
 }
