9fc4d534543d6d979438ecfc3ddebb0f917d8d08
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2021-07-04 00:19:46 +0200

    added downloads for 1.2.5 deps and client

--- src/main/java/com/enderzombi102/mception/client/BinInstaller.java
+++ src/main/java/com/enderzombi102/mception/client/BinInstaller.java
@@ -13,8 +13,10 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Objects;
 
 import static com.enderzombi102.mception.client.MCeptionClient.MCEPTION_DIR;
+import static com.enderzombi102.mception.MCeption.LOGGER;
 
 public class BinInstaller {
 
@@ -37,20 +39,23 @@ public class BinInstaller {
 		// install lwjgl
 		installIfNotPresent( getLwjgl(), BIN_DIR.resolve("lwjgl-natives.jar"), true );
 		installIfNotPresent(
-				"https://libraries.minecraft.net/org/lwjgl/lwjgl/lwjgl/2.9.4-nightly-20150209/lwjgl-2.9.4-nightly-20150209.jar",
+				"https://libraries.minecraft.net/org/lwjgl/lwjgl/lwjgl/2.9.0/lwjgl-2.9.0.jar",
 				getBinary("lwjgl")
 		);
 		// install lwjgl-util
 		installIfNotPresent(
-				"https://libraries.minecraft.net/org/lwjgl/lwjgl/lwjgl_util/2.9.4-nightly-20150209/lwjgl_util-2.9.4-nightly-20150209.jar",
+				"https://libraries.minecraft.net/org/lwjgl/lwjgl/lwjgl_util/2.9.0/lwjgl_util-2.9.0.jar",
 				getBinary("lwjgl-util")
 		);
-		// install minecraft
+		// install minecraft 1.2.5
 		installIfNotPresent(
 				"https://launcher.mojang.com/v1/objects/f690d4136b0026d452163538495b9b0e8513d718/client.jar",
 				getBinary("client-obf")
 		);
 		remapClient();
+		// remove meta inf
+		Files.deleteIfExists( BIN_DIR.resolve("META-INF").resolve("MANIFEST.MF") );
+		Files.deleteIfExists( BIN_DIR.resolve("META-INF") );
 	}
 
 	private static String getJinput() {
@@ -65,18 +70,27 @@ public class BinInstaller {
 
 	private static String getLwjgl() {
 		if ( SystemUtils.IS_OS_WINDOWS )
-			return "https://libraries.minecraft.net/org/lwjgl/lwjgl/lwjgl-platform/2.9.4-nightly-20150209/lwjgl-platform-2.9.4-nightly-20150209-natives-windows.jar";
+			return "https://libraries.minecraft.net/org/lwjgl/lwjgl/lwjgl-platform/2.9.0/lwjgl-platform-2.9.0-natives-windows.jar";
 		else if ( SystemUtils.IS_OS_MAC_OSX )
-			return "https://libraries.minecraft.net/org/lwjgl/lwjgl/lwjgl-platform/2.9.4-nightly-20150209/lwjgl-platform-2.9.4-nightly-20150209-natives-osxs.jar";
+			return "https://libraries.minecraft.net/org/lwjgl/lwjgl/lwjgl-platform/2.9.0/lwjgl-platform-2.9.0-natives-osx.jar";
 		else if ( SystemUtils.IS_OS_LINUX )
-			return "https://libraries.minecraft.net/org/lwjgl/lwjgl/lwjgl-platform/2.9.4-nightly-20150209/lwjgl-platform-2.9.4-nightly-20150209-natives-linux.jar";
+			return "https://libraries.minecraft.net/org/lwjgl/lwjgl/lwjgl-platform/2.9.0/lwjgl-platform-2.9.0-natives-linux.jar";
 		throw new IllegalStateException("OS NOT SUPPORTED! how are you running this?");
 	}
 
-	private static void installIfNotPresent(String url, Path path, boolean ...unzip ) throws IOException {
+	private static void installIfNotPresent(String url, Path path) throws IOException {
+		installIfNotPresent(url, path, path, false);
+	}
+
+	@SuppressWarnings("SameParameterValue")
+	private static void installIfNotPresent(String url, Path path, boolean unzip) throws IOException {
+		installIfNotPresent(url, path, path, unzip);
+	}
+
+	private static void installIfNotPresent(String url, Path path, Path pathToCheck, boolean unzip ) throws IOException {
 		if (! path.toFile().exists() )
 			new Download( url ).downloadTo( path );
-		if ( unzip.length > 0 )
+		if ( unzip )
 			UnzipUtility.unzip( path, path.getParent() );
 	}
 
@@ -90,7 +104,8 @@ public class BinInstaller {
 				getBinary("jutils").toFile().exists() &&
 				getBinary("lwjgl").toFile().exists() &&
 				getBinary("lwjgl-util").toFile().exists() &&
-				getBinary("client").toFile().exists();
+				getBinary("client").toFile().exists() &&
+				getBinary("client").toFile().length() >= getBinary("client-obf").toFile().length();
 
 	}
 
@@ -98,7 +113,15 @@ public class BinInstaller {
 		return BIN_DIR.resolve(bin + ".jar");
 	}
 
+	@SuppressWarnings("ResultOfMethodCallIgnored")
 	private static void remapClient() {
+		// don't remap if already done
+		if ( isInstalled() ) return;
+		if ( getBinary("client").toFile().exists() ) {
+			getBinary("client").toFile().delete();
+		}
+
+		LOGGER.info("[MCeption] Remapping client.jar");
 		TinyRemapper remapper = net.fabricmc.tinyremapper.TinyRemapper.newRemapper()
 				.withMappings( TinyRemapperMappingsHelper.create( getMappings(), "official", "named"  ) )
 				.rebuildSourceFilenames(true)
@@ -122,8 +145,8 @@ public class BinInstaller {
 	}
 
 	private static TinyTree getMappings() {
-		InputStream stream = BinInstaller.class.getResourceAsStream("mappings.tiny");
-		assert stream != null;
+		InputStream stream = BinInstaller.class.getResourceAsStream("/mappings.tiny");
+		Objects.requireNonNull(stream);
 		try {
 			return TinyMappingFactory.loadWithDetection(
 					new BufferedReader(
